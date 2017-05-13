a9e40a58d0ccf37b5eae775e1e63d004b4db2bba
Didi <didimilikina8@gmail.com>
2017-05-13 19:08:06 +0300

    new project

---
+++ Programming fundamentals/Progr. Fund. Extended - Exercises/Data Types - Numeral Types and Type Conversion - Lab/02. Circle Perimeter (12 Digits)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Circle_Perimeter__12_Digits_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{(2 * Math.PI * r):f12}");
+        }
+    }
+}
---
+++ Programming fundamentals/Progr. Fund. Extended - Exercises/Data Types - Numeral Types and Type Conversion - Lab/03. Exact Product of Real Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Exact_Product_of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal extractProduct = 0;
+            for (int i = 0; i < n; i++)
+            {
+                extractProduct *= decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine(extractProduct);
+            }
+           
+        }
+    }
+}
