151d4cd21c4d8e2ee3d9a13173516c9cefb89805
Didi <didimilikina8@gmail.com>
2017-05-14 13:56:00 +0300

    new exercise

--- Programming basics/Exams/Programming Basics Exam - 7 May 2017/05. Crown/Program.cs
+++ Programming basics/Exams/Programming Basics Exam - 7 May 2017/05. Crown/Program.cs
@@ -11,10 +11,10 @@ namespace _05.Crown
         static void Main(string[] args)
         {
             var n = int.Parse(Console.ReadLine());
-            var width = 2 * n + 1;
+            var width = 2 * n - 1;
             var height = n / 2 + 4;
 
-            var firstEmpties = n - 2;
+            var firstEmpties = (width - 3) / 2;
             Console.Write('@');
             Console.Write(new string(' ', firstEmpties));
             Console.Write('@');
@@ -22,14 +22,14 @@ namespace _05.Crown
             Console.WriteLine('@');
 
             Console.Write("**");
-            Console.Write(new string(' ', n/2 + 1));
+            Console.Write(new string(' ', firstEmpties - 1));
             Console.Write('*');
-            Console.Write(new string(' ', n/2 + 1));
+            Console.Write(new string(' ', firstEmpties -1));
             Console.WriteLine("**");
 
             var secondEmpties = n - 5;
             var pointsMiddle = 1;
-            for (int i = 1; i <= n / 2 - 1; i++)
+            for (int i = 1; i <= n / 2 - 2; i++)
             {
                 Console.Write('*');
                 Console.Write(new string('.', i));
@@ -46,15 +46,23 @@ namespace _05.Crown
                 secondEmpties -= 2;
             }
 
-            var stars = width - 2 * pointsMiddle - 3;
-
+            var points = (n - 3)/ 2;
             Console.Write('*');
-            Console.Write(new string('.', pointsMiddle + 1));
-            Console.Write(new string('*', ));
-            Console.Write('.');
-            Console.Write(new string('*', ));
-            Console.Write(new string('.', pointsMiddle + 1));
+            Console.Write(new string('.', points + 1));
+            Console.Write('*');
+            Console.Write(new string('.', n-3));
+            Console.Write('*');
+            Console.Write(new string('.', points + 1));
             Console.WriteLine('*');
+
+           var stars = width - 2 * (n/2) - 3;
+           Console.Write('*');
+           Console.Write(new string('.', n / 2));
+           Console.Write(new string('*', stars / 2));
+           Console.Write('.');
+           Console.Write(new string('*', stars / 2));
+           Console.Write(new string('.', n / 2));
+           Console.WriteLine('*');
                 
 
             for (int i = 0; i < 2; i++)
