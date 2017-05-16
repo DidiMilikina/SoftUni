59676a22fb67e9ce37e06dc7038184673f164d5d
Didi <didimilikina8@gmail.com>
2017-05-16 17:42:25 +0300

    new exercises without num 5

---
+++ Programming fundamentals/Progr. Fund. Extended - Exercises/Data Types - Text and Other Types, Variables - Lab/02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        char firstLetter = (char)(i + 'a');
+                        char secondLetter = (char)(j + 'a');
+                        char thirdLetter = (char)(k + 'a');
+                        Console.WriteLine($"{firstLetter}{secondLetter}{thirdLetter}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Programming fundamentals/Progr. Fund. Extended - Exercises/Data Types - Text and Other Types, Variables - Lab/05. Refactor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Refactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int total = 0;
+            int something = 0;
+            bool toe = false;
+            
+            for (int i = 1; i <= number; i++)
+            {
+                while(i >0)
+                {
+                    total += i % 10;
+                    i = i / 10;
+                }
+                var sum = (total == 5) || (total == 7) || (total == 11);
+                if(toe = sum)
+                {
+                    Console.WriteLine(${);
+                }
+            }
+        }
+    }
+}
