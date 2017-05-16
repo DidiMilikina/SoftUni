0acfe9b89291ce91a015d80aa4312c27e33dbcdc
Didi <didimilikina8@gmail.com>
2017-05-16 21:41:21 +0300

    all exercises

---
+++ Programming fundamentals/Progr. Fund. Extended - Exercises/Data Types - Text and Other Types, Variables/05.Ref Special Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Ref_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                int sum = 0;
+                var currentNum = i;
+                while (currentNum > 0)
+                {
+                    sum += currentNum % 10;
+                    currentNum = currentNum / 10;
+                }
+
+                var result = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{i} -> {result}");
+            }
+        }
+    }
+}
