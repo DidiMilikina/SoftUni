fed3c04d1f97343d044128e794c5a27fed2b469f
didi <didimilikina8@gmail.com>
2017-05-20 17:39:20 +0300

    added new project

---
+++ Programming fundamentals/Exercises/Data Types - Exercise/FromTerabytesТоBits/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FromTerabytesТоBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double terabytes = double.Parse(Console.ReadLine());
+            Console.WriteLine(terabytes * 1024 * 1024 * 1024 * 1024 * 8);
+        }
+    }
+}
