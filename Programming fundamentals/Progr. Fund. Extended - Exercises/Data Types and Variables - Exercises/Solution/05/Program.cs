ae8f3fe52aceac5a86c757e4e2ed3f12dbb61eea
didi <didimilikina8@gmail.com>
2017-05-20 17:02:40 +0300

    added new folder

---
+++ Programming fundamentals/Progr. Fund. Extended - Exercises/Data Types and Variables - Exercises/Solution/05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var distanceNearestStar = 4.22;
+            var distanceToTheCenter = 26000;
+            var diameterOfTheMilkyWay = 100000;
+            var distanceFromTheEarth = 46500000000;
+
+        }
+    }
+}
---
+++ Programming fundamentals/Progr. Fund. Extended - Exercises/Data Types and Variables - Exercises/Solution/07/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double terabytes = double.Parse(Console.ReadLine());
+           
+            Console.WriteLine(terabytes * 1024 * 1024 * 1024 * 1024 * 8);
+        }
+    }
+}
