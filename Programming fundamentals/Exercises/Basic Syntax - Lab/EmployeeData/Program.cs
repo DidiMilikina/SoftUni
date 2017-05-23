99a90d6afa72839fea8b33570e28aa77eb1a4800
didi <didimilikina8@gmail.com>
2017-05-23 15:58:48 +0300

    Basic Syntax - Lab - complete

---
+++ Programming fundamentals/Exercises/Basic Syntax - Lab/EmployeeData/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int id = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+            var employeeId = id.ToString().PadLeft(8, '0');
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Employee ID: {employeeId}");
+            Console.WriteLine($"Salary: {salary:f2}");
+        }
+    }
+}
