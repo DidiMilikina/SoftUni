dd35db465c5fc44f8889d7d2959661490e4fad40
didi <didimilikina8@gmail.com>
2017-05-24 23:42:14 +0300

    Exercise 1-4 complete

--- Programming fundamentals/Exercises/Basic Syntax/BeverageLabels/Program.cs
+++ Programming fundamentals/Exercises/Basic Syntax/BeverageLabels/Program.cs
@@ -10,6 +10,17 @@ namespace BeverageLabels
     {
         static void Main(string[] args)
         {
+            string name = Console.ReadLine();
+            double volume = double.Parse(Console.ReadLine());
+            double energyContent = double.Parse(Console.ReadLine());
+            double sugarContent =double.Parse(Console.ReadLine());
+
+            double volumeMultiplier = volume / 100;
+
+            Console.Write($"{volume}ml ");
+            Console.WriteLine($"{name}: ");
+            Console.Write($"{energyContent * volumeMultiplier}kcal, ");
+            Console.WriteLine($"{sugarContent * volumeMultiplier}g sugars");
         }
     }
 }
