ed6c9c853674fdee31b5c3ddf43e000f3d2a8efd
didi <didimilikina8@gmail.com>
2017-05-26 21:07:27 +0300

    new project

---
+++ Programming fundamentals/Lab/Conditional Statements and Loops/MultiplictionTable2.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplictionTable2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int times = int.Parse(Console.ReadLine());
+            for (int i = number; i <= 10; i++)
+            {
+               
+                
+                    int sum = number * times;
+                    Console.WriteLine($"{number} X {times} = {sum}");
+                
+            }
+        }
+    }
+}
---
+++ Programming fundamentals/Lab/Conditional Statements and Loops/OddNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = n; i < ; i++)
+            {
+                
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine("The number is: {}");
+                }
+                else
+                {
+                    Console.WriteLine("Please write an odd number.");
+                }
+            }
+
+        }
+    }
+}
