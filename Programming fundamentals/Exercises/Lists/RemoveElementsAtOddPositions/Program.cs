bb8e5999c1448676a1f7e019bd50c15c68a76d18
didi <didimilikina8@gmail.com>
2017-06-09 01:36:45 +0300

    Lists - Exercises(only ex. 1,2)

---
+++ Programming fundamentals/Exercises/Lists/RemoveElementsAtOddPositions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveElementsAtOddPositions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine()
+                .Split(' ')
+                .ToList();
+            var result = new List<string>();
+            for (int i = 1; i < name.Count; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    result.Add(name[i]);
+                }
+            }
+            Console.WriteLine(String.Join("", result));
+        }
+    }
+}
---
+++ Programming fundamentals/Exercises/Lists/TrackDownloader/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var blackList = Console.ReadLine()
+                .Split(' ');
+
+            var downloadedSongs = new List<string>();
+            var line = Console.ReadLine();
+            while (line != "end")
+            {
+                var isInBlackList = false;
+                foreach (var blackListedWord in blackList)
+                {
+                    if (line.Contains(blackListedWord))
+                    {
+                        isInBlackList = true;
+                        break;
+                    }
+                }
+                if (!isInBlackList)
+                {
+                    downloadedSongs.Add(line);
+                }
+
+                line = Console.ReadLine();
+            }
+
+            downloadedSongs.Sort();
+
+            // Console.WriteLine(string.Join(Environment.NewLine, downloadedSongs));
+
+            foreach (var track in downloadedSongs)
+            {
+                Console.WriteLine(track);
+            }
+        }
+    }
+}
