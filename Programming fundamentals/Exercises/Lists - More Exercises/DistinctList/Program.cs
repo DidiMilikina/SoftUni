2ce065480517c57d13358b49fa2287b17e2cfafb
didi <didimilikina8@gmail.com>
2017-06-10 11:58:10 +0300

    New project - Lists - More exercises

---
+++ Programming fundamentals/Exercises/Lists - More Exercises/DistinctList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistinctList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+              var numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            var counts = new int();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if ()
+                {
+                    
+                }
+            } 
+        }
+    }
+}
