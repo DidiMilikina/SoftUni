234acc8288d92011554bc0ed2ce18bac4bdf7dcb
didi <didimilikina8@gmail.com>
2017-06-11 12:32:44 +0300

    Data Types - Exercises(without ex. 10*, 17**)

--- Programming fundamentals/Exercises/Data Types - Exercise/CypherRoulette/Program.cs
+++ Programming fundamentals/Exercises/Data Types - Exercise/CypherRoulette/Program.cs
@@ -10,6 +10,8 @@ namespace CypherRoulette
     {
         static void Main(string[] args)
         {
+
+
         }
     }
 }
--- Programming fundamentals/Exercises/Data Types - Exercise/DataOverflow/Program.cs
+++ Programming fundamentals/Exercises/Data Types - Exercise/DataOverflow/Program.cs
@@ -10,6 +10,8 @@ namespace DataOverflow
     {
         static void Main(string[] args)
         {
+
+
         }
     }
 }
