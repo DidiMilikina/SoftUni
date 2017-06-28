4c0f90be3160a2e13137dd715921e7ea0f445287
didi <didimilikina8@gmail.com>
2017-06-28 17:51:20 +0300

    Dictionaries - Exercises(only ex. 1)

---
+++ Programming fundamentals/Exercises/Dictionaries/Dict-Ref/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dict_Ref
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+
+            while (line != "end")
+            {
+                var firstElement = ;
+
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Programming fundamentals/Exercises/Dictionaries/LetterRepetition/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterRepetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputLetters = Console.ReadLine();
+
+            var lettersCount = new Dictionary<char, int>();
+
+            foreach (var letter in inputLetters)
+            {
+                if (!lettersCount.ContainsKey(letter))
+                {
+                    lettersCount[letter] = 0;
+                }
+                      lettersCount[letter]++;   
+                             
+            }
+            
+            foreach (var pair in lettersCount)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
