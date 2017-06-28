6288deddaf38e805bcd804e13bf75a640c9d0f2c
didi <didimilikina8@gmail.com>
2017-06-28 14:58:06 +0300

    Dictionaries - Lab - complete

---
+++ Programming fundamentals/Lab/Dictionaries/CountRealNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+            var result = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!result.ContainsKey(number))
+                {
+                    result[number] = 0;
+                }
+                result[number]++;
+            }
+
+            foreach (var kv in result)
+            {
+                Console.WriteLine($"{kv.Key} -> {kv.Value} times");
+            }
+        }
+    }
+}
---
+++ Programming fundamentals/Lab/Dictionaries/OddOccurrences/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine()
+                .ToLower()
+                .Split(' ');
+
+            var result = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (!result.ContainsKey(word))
+                {
+                    result[word] = 0;
+                }
+                result[word]++;
+            }
+
+            var oddNumber = new List<string>();
+
+            foreach (var kv in result)
+            {
+                var value = kv.Value;
+                if (value % 2 != 0)
+                {
+                    oddNumber.Add(kv.Key);
+                }
+            }
+            Console.WriteLine(string.Join(", ", oddNumber));
+        }
+    }
+}
