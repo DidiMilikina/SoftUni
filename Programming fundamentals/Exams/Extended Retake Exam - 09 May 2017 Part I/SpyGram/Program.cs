22a31ab8aa38814fb402a537667c8f9af7743ac4
didi <didimilikina8@gmail.com>
2017-07-07 12:10:51 +0300

    Extended Retake Exam - 09 May 2017 Part I(without ex. 2)

---
+++ Programming fundamentals/Exams/Extended Retake Exam - 09 May 2017 Part I/SpyGram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpyGram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string privateKey = Console.ReadLine();
+
+            while (privateKey != "END")
+            {
+                
+            }
+        }
+    }
+}
