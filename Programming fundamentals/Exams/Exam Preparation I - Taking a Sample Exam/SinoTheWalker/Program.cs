52e8f45b0bc630fe08833993929f6f4113eafcd2
didi <didimilikina8@gmail.com>
2017-07-07 15:24:08 +0300

    Exam Preparation I - Taking a Sample Exam(without ex. 3, 4)

---
+++ Programming fundamentals/Exams/Exam Preparation I - Taking a Sample Exam/SinoTheWalker/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinoTheWalker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timeFormat = @"HH\:mm\:ss";
+            var leavingTime = 
+                DateTime.ParseExact(Console.ReadLine(), timeFormat, CultureInfo.InvariantCulture);
+
+            var numberOfSteps = decimal.Parse(Console.ReadLine());
+            var timeInSecondsPerStep = decimal.Parse(Console.ReadLine());
+
+            var secondsPerDay = 60 * 60 * 24;
+            var totalSecondsNeeded = (int)(numberOfSteps * timeInSecondsPerStep % secondsPerDay);
+            var arrivalTime = leavingTime.AddSeconds(totalSecondsNeeded);
+
+            Console.WriteLine($"Time Arrival: {arrivalTime.ToString(timeFormat)}");
+        }
+    }
+}
---
+++ Programming fundamentals/Exams/Exam Preparation I - Taking a Sample Exam/SoftUniKaraoke/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniKaraoke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var partcipants = Console.ReadLine()
+                .Split(',')
+                .Select(a => a.Trim())
+                .ToArray();
+
+            var songs = Console.ReadLine()
+                .Split(',')
+                .Select(a => a.Trim())
+                .ToArray();
+
+            var participantAwards = new Dictionary<string, SortedSet<string>>();
+
+            var line = Console.ReadLine();
+
+            while (line != "dawn")
+            {
+                var tokens = line
+                    .Split(',')
+                    .Select(a => a.Trim())
+                    .ToArray();
+
+                var participant = tokens[0];
+                var song = tokens[1];
+                var award = tokens[2];
+
+                if (songs.Contains(song) && partcipants.Contains(participant))
+                {
+                    if (!participantAwards.ContainsKey(participant))
+                    {
+                       participantAwards[participant] = new SortedSet<string>();
+                        //sort awards in alphabetical order
+                    }
+                     participantAwards[participant].Add(award);
+                }
+
+                line = Console.ReadLine();
+            }
+            if (!participantAwards.Any())
+            {
+                Console.WriteLine("No awards");
+                return;
+            }
+            
+            var sortedPartcipantAwards = participantAwards
+                .OrderByDescending(a => a.Value.Count)
+                //if we have more than 1 participant with same awards like other participant we should ... 
+                .ThenBy(a => a.Key)
+                //sort participants in alphabetical order 
+                .ToDictionary(a => a.Key, a => a.Value);
+
+            foreach (var partcipantAward in sortedPartcipantAwards)
+            {
+                var partcipant = partcipantAward.Key;
+                var awards = partcipantAward.Value;
+
+                Console.WriteLine($"{partcipant}: {awards.Count} awards");
+                foreach (var award in awards)
+                {
+                    Console.WriteLine($"--{award}");
+                }
+            }
+        }
+    }
+}
