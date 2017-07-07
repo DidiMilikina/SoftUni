236e7894fb7257d57843073b2bf5bdcd04f72f50
didi <didimilikina8@gmail.com>
2017-07-07 12:37:42 +0300

    Extended Exam - 30 April 2017 Part I(without ex. 2)

---
+++ Programming fundamentals/Exams/Extended Exam - 30 April 2017 Part I/WormIpsum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WormIpsum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+            List<string> validInput = new List<string>();
+
+            while (inputLine != "Worm Ipsum")
+            {
+                
+            }
+        }
+    }
+}
---
+++ Programming fundamentals/Exams/Extended Exam - 30 April 2017 Part I/Wormtest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wormtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lenghtInMeters = int.Parse(Console.ReadLine());
+            double widthInCentimeters = double.Parse(Console.ReadLine());
+
+            lenghtInMeters *= 100;
+            double remaining = lenghtInMeters % widthInCentimeters;
+
+            if (widthInCentimeters == 0)
+            {
+                Console.WriteLine($"{lenghtInMeters * widthInCentimeters:f2}");
+            }
+            else
+            {
+                if (remaining != 0)
+                {
+                    double inPercents = (lenghtInMeters / widthInCentimeters) * 100;
+                    Console.WriteLine($"{inPercents:f2}%");
+                }
+                else
+                {
+                    Console.WriteLine($"{lenghtInMeters * widthInCentimeters:f2}");
+                }
+            }
+        }
+    }
+}
