763f7f918ad20c4ee72a769672c183b940796d04
didi <didimilikina8@gmail.com>
2017-07-07 23:40:38 +0300

    Exam Preparation III - Taking a Sample Exam(without ex. 2, 3, 4)

---
+++ Programming fundamentals/Exams/Exam Preparation III - Taking a Sample Exam/SoftuniCoffeeOrders/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftuniCoffeeOrders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOrders = int.Parse(Console.ReadLine());
+            var totalSum = 0.0M;
+
+            for (int i = 0; i < countOrders; i++)
+            {
+                decimal pricePerCapsule = decimal.Parse(Console.ReadLine());
+
+                var date = 
+                    DateTime.ParseExact(Console.ReadLine(), "d/M/yyyy", CultureInfo.InvariantCulture);
+
+                long capsulesCount = long.Parse(Console.ReadLine());
+                var month = date.Month;
+                var year = date.Year;
+                int days = DateTime.DaysInMonth(year, month);
+
+                var price = ((days * capsulesCount) * pricePerCapsule);
+                totalSum += price;
+
+                Console.WriteLine($"The price for the coffee is: ${price:f2}");
+            }
+
+            Console.WriteLine($"Total: ${totalSum:f2}");
+        }
+    }
+}
