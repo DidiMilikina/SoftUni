a507bb7219f6aefe9191599fad7d491745cdf185
didi <didimilikina8@gmail.com>
2017-07-08 12:11:49 +0300

    Exam Preparation V - Taking a Sample Exam(without ex. 2, 3, 4)

---
+++ Programming fundamentals/Exams/Exam Preparation V - Taking a Sample Exam/HornetWings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HornetWings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wingFlaps = int.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            int endurance = int.Parse(Console.ReadLine());
+
+            var totalDistance = wingFlaps / 1000 * distance;
+            var seconds = wingFlaps / 100;
+            var rest = wingFlaps / endurance * 5;
+            var restInSeconds = seconds + rest;
+
+            Console.WriteLine($"{totalDistance:f2} m.");
+            Console.WriteLine($"{restInSeconds} s.");
+
+        }
+    }
+}
