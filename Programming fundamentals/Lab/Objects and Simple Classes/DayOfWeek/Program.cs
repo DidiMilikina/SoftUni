08c6a9e11a447624bb904ee01687d1923a0ad2c8
didi <didimilikina8@gmail.com>
2017-07-14 21:23:36 +0300

    Objects and Simple Classes - Lab(without ex. 4, 5, 6, 7)

---
+++ Programming fundamentals/Lab/Objects and Simple Classes/DayOfWeek/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = Console.ReadLine();
+
+            var datTime = DateTime.ParseExact(date,
+                "d-M-yyyy",
+                CultureInfo.InvariantCulture);
+
+            Console.WriteLine(datTime.DayOfWeek);
+        }
+    }
+}
---
+++ Programming fundamentals/Lab/Objects and Simple Classes/RandomizeWords/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            var random = new Random();
+            var rnd = random.Next(0, words.Length);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                var word = words[i];
+                if (i != rnd)
+                {
+                    var tempWord = words[rnd];
+                    words[rnd] = word;
+                    words[i] = tempWord;
+                }
+            }
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
