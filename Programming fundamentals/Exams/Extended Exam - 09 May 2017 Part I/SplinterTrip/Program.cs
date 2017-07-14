eca517410d4d99e8b77a5e46d120ae4e398e6774
didi <didimilikina8@gmail.com>
2017-07-14 22:01:45 +0300

    Programming Fundamentals Extended Retake Exam - 09 May 2017 Part I(withou ex. 2)

---
+++ Programming fundamentals/Exams/Extended Exam - 09 May 2017 Part I/SplinterTrip/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplinterTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tripDistanceInMiles = double.Parse(Console.ReadLine());
+            double fuelTankCapacityInLiters = double.Parse(Console.ReadLine());
+            double milesSpentInHeavyWinds = double.Parse(Console.ReadLine());
+
+            double milesInNonHeavyWinds = tripDistanceInMiles - milesSpentInHeavyWinds;
+            double nonHeavyConsumption = milesInNonHeavyWinds * 25;
+            double heavyWindsConsumption = milesSpentInHeavyWinds * (25 * 1.5);
+            double fuelConsumption = nonHeavyConsumption + heavyWindsConsumption;
+            double tolerance = fuelConsumption * 0.05;
+            double totalFuelConsumption = fuelConsumption + tolerance;
+
+            Console.WriteLine($"Fuel needed: {totalFuelConsumption:f2}L");
+            if (fuelTankCapacityInLiters >= totalFuelConsumption)
+            {
+                double enough = fuelTankCapacityInLiters - totalFuelConsumption;
+                Console.WriteLine($"Enough with {enough:f2}L to spare!");
+            }
+            else
+            {
+                double need = totalFuelConsumption - fuelTankCapacityInLiters;
+                Console.WriteLine($"We need {Math.Abs(need):f2}L more fuel.");
+            }
+        }
+    }
+}
