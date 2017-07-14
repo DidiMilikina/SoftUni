9d0141c52124b8396948ce7abb67289d793e5434
didi <didimilikina8@gmail.com>
2017-07-14 21:36:53 +0300

    Data Types: Numeral Types and Type Conversion - Lab - complete

---
+++ Programming fundamentals/Lab/Data Types - Numeral Тypes/RefactorVolumeOfPyramid/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorVolumeOfPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Length: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            double height = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            var volumeOfPyramid = (width * length * height) / 3;
+
+            Console.WriteLine($"Pyramid Volume: {volumeOfPyramid:f2}");
+        }
+    }
+}
---
+++ Programming fundamentals/Lab/Data Types - Numeral Тypes/TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriplesOfLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        char firstLetter = (char)(i + 'a');
+                        char secondLetter = (char)(j + 'a');
+                        char thirdLetter = (char)(k + 'a');
+                        Console.WriteLine($"{firstLetter}{secondLetter}{thirdLetter}");
+                    }
+                }
+            }
+        }
+    }
+}
