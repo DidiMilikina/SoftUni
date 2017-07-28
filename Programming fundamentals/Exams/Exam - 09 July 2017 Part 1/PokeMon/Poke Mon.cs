cb6859272e388ed0ab41aabf8db36265e52fda35
didi <didimilikina8@gmail.com>
2017-07-28 12:16:48 +0300

    Exam - 09 July 2017 Part I(withou ex. 2)

---
+++ Programming fundamentals/Exams/Exam - 09 July 2017 Part 1/PokeMon/Poke Mon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int pokePower = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+
+            var reminder = pokePower / 2;
+            var temp = 0;
+            var counter = 0;
+
+            while (pokePower >= distance)
+            {
+                temp = pokePower - distance;
+                if (temp == reminder && exhaustionFactor != 0)
+                {
+                    temp = temp / exhaustionFactor;
+                }
+                pokePower = temp;
+                counter++;
+            }
+
+            Console.WriteLine(pokePower);
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ Programming fundamentals/Exams/Exam - 09 July 2017 Part 1/PokemonDon'tGo/Pokemon Don't Go.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonDon_tGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] {' '},
+                    StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                .ToList();
+
+            while (numbers.Count >= 1)
+            {
+                var indexNumber = Console.ReadLine();
+                if (indexNumber < 0)
+                {
+                    
+                }
+            }
+        }
+    }
+}
