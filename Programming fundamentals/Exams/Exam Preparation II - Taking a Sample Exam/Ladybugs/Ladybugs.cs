6510cb03a61159953bcaa973808e47b21c3a4448
didi <didimilikina8@gmail.com>
2017-08-06 11:32:47 +0300

    Exam Preparation II - complete

---
+++ Programming fundamentals/Exams/Exam Preparation II - Taking a Sample Exam/Ladybugs/Ladybugs.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladybugs
+{
+    class Ladybugs
+    {
+        static void Main(string[] args)
+        {
+            int sizeField = int.Parse(Console.ReadLine());
+            var indexes = Console.ReadLine()
+                .Split(new[] { ' ' },
+                    StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var field = new int[sizeField];
+            string input;
+            foreach (var ladybugIndex in indexes)
+            {
+                if (ladybugIndex < 0 || ladybugIndex >= sizeField)
+                {
+                    continue;
+                }
+                field[ladybugIndex] = 1;
+            }
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                var command = input
+                    .Split(new[] { ' ' },
+                        StringSplitOptions.RemoveEmptyEntries);
+
+                var currentIndex = int.Parse(command[0]);
+                var direction = command[1];
+                var flyLength = int.Parse(command[2]);
+
+                if (currentIndex < 0 || currentIndex >= sizeField)
+                {
+                    continue;
+                }
+                if (field[currentIndex] == 0)
+                {
+                    continue;
+                }
+
+                field[currentIndex] = 0;
+                var position = currentIndex;
+
+                // for each movement 
+                while (true)
+                {
+                    if (direction == "right")
+                    {
+                        position += flyLength;
+                    }
+                    else
+                    {
+                        position -= flyLength;
+                    }
+
+                    if (position < 0 || position >= sizeField)
+                    {
+                        break;
+                    }
+
+                    //If there is another ladybug
+                    if (field[position] == 1)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        field[position] = 1;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", field));
+        }
+    }
+}
---
+++ Programming fundamentals/Exams/Exam Preparation II - Taking a Sample Exam/NetherRealms/Nether Realms.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NetherRealms
+{
+    public class Demon
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public double Damage { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var demons = Console.ReadLine()
+                .Split(new[] { ' ', ',' },
+                    StringSplitOptions.RemoveEmptyEntries);
+            var regex = new Regex(@"-?\d+\.?\d*");
+            var result = new SortedDictionary<string, Demon>();
+
+            foreach (var demon in demons)
+            {
+                var healthSymbols = demon.Where(s =>
+                    !char.IsDigit(s) &&
+                    s != '+' &&
+                    s != '-' &&
+                    s != '*' &&
+                    s != '/' &&
+                    s != '.');
+
+                var health = 0;
+                foreach (var healthSymbol in healthSymbols)
+                {
+                    health += healthSymbol;
+                }
+
+                var matches = regex.Matches(demon);
+                var damage = 0.0;
+
+                foreach (Match match in matches)
+                {
+                    var currentNumber = double.Parse(match.Value);
+                    damage += currentNumber;
+                }
+
+                var modifiers = demon.Where(s => s == '*' || s == '/');
+                foreach (var modifier in modifiers)
+                {
+                    if (modifier == '*')
+                    {
+                        damage *= 2;
+                    }
+                    else
+                    {
+                        damage /= 2;
+                    }
+                }
+
+                result.Add(demon, new Demon
+                {
+                    Name = demon,
+                    Health = health,
+                    Damage = damage
+                });
+            }
+
+            foreach (var demonKVP in result)
+            {
+                var demon = demonKVP.Value;
+                Console.WriteLine($"{demon.Name} - {demon.Health} health, {demon.Damage:f2} damage ");
+            }
+        }
+    }
+}
