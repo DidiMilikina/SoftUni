dbeaf695809cfc54b9f3fdc44e4f7556e9d65215
didi <didimilikina8@gmail.com>
2018-02-13 19:22:10 +0200

    Added new folder.

--- JS Core/Lab Functions and Arrow Functions/02. Square of Stars.js
+++
@@ -1,8 +0,0 @@
-function squareOfStars(count = 5) {
-    function printStars(n = count) {
-        console.log("*" + " *".repeat(n - 1));
-    }
-    for (let i = 1; i <= count; i++)
-        printStars();
-}
-//squareOfStars(3);
